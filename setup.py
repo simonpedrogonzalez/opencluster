6e89260642569da75bbcc53aff8e3a4d995e560a
Simón Pedro <simon.pedro.g@gmail.com>
2020-10-06 09:50:28 -0300

    tox configuration

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup
+
+
+# Utility function to read the README file.
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="opencluster",
+    version="0.0.1",
+    author="Simón Pedro González, Antonio Alejo",
+    author_email="simon.pedro.g@gmail.com",
+    description=(
+        "mempership probabilities for open star clusters"
+    ),
+    license="GNU3",
+    keywords="star cluster membership probabilities",
+    url="http://packages.python.org/opencluster",
+    packages=["opencluster", "tests"],
+    long_description=read("README.md"),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Scientific/Engineering :: Astronomy",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+    install_requires=["numpy",
+                      "scikitlearn",
+                      "matplotlib",
+                      "scipy",
+                      "sklearn"],
+)
