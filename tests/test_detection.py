d810eca8063419b32b690c6094aa6448f295e34f
Simón Pedro <simon.pedro.g@gmail.com>
2021-10-13 08:14:47 -0300

    test detection files

---
+++ tests/test_detection.py
@@ -0,0 +1,19 @@
+from opencluster.synthetic import EDSD, polar_to_cartesian, cartesian_to_polar, is_inside_circle, UniformCircle, UniformSphere, Cluster, Field, Synthetic
+from scipy.stats import kstest, multivariate_normal
+import pandas as pd
+import math
+import numpy as np
+import pytest
+
+class TestDetection:
+    def test_histogram():
+        # histogram(data, bin_shape: list)
+        return False
+
+    def test_convolve():
+        # convolve()
+        return False
+
+    def test_find_clusters():
+        # find_clusters
+        return False
