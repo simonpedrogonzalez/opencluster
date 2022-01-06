621bed6ac0930a7f6495fd9be6b99ca21a6839e6
Simón Pedro González <sgonzalez@travelpaq.com.ar>
2022-01-06 10:39:07 -0300

    added colnames as utils

--- opencluster/utils.py
+++ opencluster/utils.py
@@ -2,6 +2,73 @@ import numpy as np
 import itertools
 from attr import attrs
 from typing import List
+from ordered_set import OrderedSet
+from typing import Optional, Tuple, List, Union, Callable, Type, Optional
+
+
+@attrs(auto_attribs=True, init=False)
+class Colnames2:
+    names: OrderedSet
+
+    def __init__(self, names: List[str]):
+        self.names = OrderedSet(names)
+
+    def exclude(self, names: Union[list, str]):
+        names = names.parse_to_list()
+        return list(self.names - OrderedSet(names))
+
+    def get_error_names(self, names: Union[list, str]=None):
+        errors = [name for name in list(self.names) if name.endswith('_error')]
+        if names is None:
+            return errors
+        names = self.parse_to_list(names)
+        sorted_errors = []
+        for name in names:
+            for err in errors:
+                if err.startswith(name):
+                    sorted_errors.append(err)
+                    errors.remove(err)
+                    break
+
+        missing_errors = len(names) != len(sorted_errors)
+
+        return sorted_errors, missing_errors
+
+    def get_corr_names(self, names: Union[list, str]=None):
+        correlations = [name for name in list(self.names) if name.endswith('_corr')]
+        if names is None:
+            return correlations
+        
+        names = self.parse_to_list(names)
+        
+        names_with_corr = []
+        for name in names:
+            for corr in correlations:
+                if name in corr:
+                    names_with_corr.append(name)
+                    break
+        names_with_corr = list(OrderedSet(names_with_corr))
+
+        len_nwc = len(names_with_corr)
+        if len_nwc == 0:
+            return [], True
+        corr_matrix = np.ndarray(shape=(len_nwc, len_nwc), dtype=f'|S{max([len(name) for name in names_with_corr + correlations])}')
+        
+        for i1, var1 in enumerate(names_with_corr):
+            for i2, var2 in enumerate(names_with_corr):
+                corr1 = f'{var1}_{var2}_corr'
+                corr2 = f'{var2}_{var1}_corr'
+                corr = corr1 if corr1 in correlations else corr2 if corr2 in correlations else ''
+                corr_matrix[i1, i2] = corr
+        
+        sorted_correlations = list(corr_matrix[np.tril_indices(vc_count, k=-1)].astype(str))
+        missing_correlations = any(name == '' for name in sorted_correlations)
+        return sorted_correlations, missing_correlations 
+
+    def parse_to_list(self, names: Union[list, str]):
+        if isinstance(names, str):
+            names = [names]
+        return names
 
 
 @attrs(auto_attribs=True, init=False)
@@ -85,6 +152,35 @@ def get_colnames(colnames: list):
 
     return [f'{var_name}_error' for var_name in var_colnames]
 
+def indices(arrays: Union[tuple, np.ndarray]):
+    
+    if isinstance(arrays, np.ndarray):
+        if len(arrays.shape) == 1:
+            return tuple(arrays)
+        else:
+            return tuple(map(tuple, tuple(arrays)))
+
+    
+    arrays = list(arrays)
+    shape = None
+    for i, arg in enumerate(arrays):
+        if isinstance(arg, np.ndarray):
+            if len(arg.shape) != 1:
+                raise ValueError('arrays must be of 1 dimension')
+            if shape is not None and shape != arg.shape:
+                raise ValueError('arrays must have same shape')
+            shape = arg.shape
+            arrays[i] = arg.astype(int)
+    
+    for i, arg in enumerate(arrays):
+        if isinstance(arg, int):
+            arrays[i] = np.ones(shape, dtype=int) * arg
+        
+    return tuple(map(tuple, np.vstack(tuple(arrays))))
+
+""" def args2r(*args, **kwargs):
+    # only works for simple 
+    return ", ".join(f"{key}={value}" for key, value in kwargs.items()) """
 """ 
 c = Colnames([
                 'ra', 'dec', 'ra_error', 'dec_error', 'ra_dec_corr',
@@ -95,3 +191,6 @@ c = Colnames([
             ])
 
 print('coso') """
+
+""" thing = args2str("algo", 12, [3,4,5], False, coso="'algomas'", otrocoso=[1,2,3], tercercoso=np.array([1,2,3]))
+print(thing) """
